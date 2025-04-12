79815a69fae7254bdfe45a6edc41c7409ceb35b6
Priya <priya@Priyas-MacBook-Pro.local>
2025-04-11 22:23:27 -0400

    cuda warning

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 import numpy as np
 import tensorflow as tf
@@ -42,4 +43,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
